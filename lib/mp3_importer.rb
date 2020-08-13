c57c127c93fffc797434fa5bc956a6f1f836687f
Leslie G <lesliegonzalez@me.com>
2020-08-13 02:54:47 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,18 +2,16 @@ class MP3Importer
   
   attr_accessor :song, :artist
   
-  def initialize(directory)
+  def initialize(files)
     @path = path
     Song << filename
   end
   
   def self.files
-    
+    files = Dir.glob("*.mp3")
   end
   
   def self.import
-    files = Dir.glob("*.jpg")
-    
     
   end
     
